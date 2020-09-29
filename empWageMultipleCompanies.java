f9295501955e44534ff73ad8b3a8ead928fda4f9
TusharPatil010010 <tusharp010010@gmail.com>
2020-09-29 21:39:59 +0530

    Added ability to save the total wage for each company

--- empWageMultipleCompanies.java
+++ empWageMultipleCompanies.java
@@ -1,22 +1,31 @@
-public class empWageMultipleCompanies {
-	
-	//Global constants
+public class empWageComputation {
 	public static final int is_part_time = 1;
 	public static final int is_full_time = 2;
 	
+	private final String company;
+	private final int emp_wage_per_hr;
+	private final int max_working_days;
+	private final int max_hrs_in_month;
+	private int total_empWage = 0;
+	
+	public empWageCalculator(String company, int emp_wage_per_hr, int max_working_days, int max_hrs_in_month) {
+		this.company = company;
+		this.emp_wage_per_hr = emp_wage_per_hr;
+		this.max_working_days = max_working_days;
+		this.max_hrs_in_month = max_hrs_in_month;
+	}
+	
 	//Method to calculate employee wage
-	public static int calculateEmpWage(String company, int emp_wage_per_hr,int max_working_days,int max_hrs_in_month) {
+	public void calculateEmpWage() {
 		//variables
 		int empHrs = 0;
 		int total_empHrs = 0;
 		int total_working_days = 0;
-		int total_empWage = 0;
 		
 		//computation
 		while(total_empHrs <= max_hrs_in_month && total_working_days < max_working_days) {
 			total_working_days++;
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-			
 			switch(empCheck) {
 			case is_part_time:
 				empHrs = 8;
@@ -32,20 +41,28 @@ public class empWageMultipleCompanies {
 			System.out.println("Day : "+total_working_days+" Emp Hrs : "+empHrs );
 		}
 		
-		//if total hrs exceeds max hrs per month then computing for max hrs per month
+		/*
+		if(total_empHrs >= max_hrs_in_month)
+			total_empHrs = max_hrs_in_month;															//if total hrs exceeds max hrs per month then computing for max hrs per month
+		*/
 		
-		System.out.println("Total emp hrs : "+total_empHrs);
 		total_empWage = total_empHrs * emp_wage_per_hr;
-		System.out.println("Total Emp wage for Company '"+company+"' is : "+total_empWage);
-				
-		return total_empWage;
+	}
+	
+	//@override
+	public String toString() {
+		return "Total Emp Wage for company '"+company+"' is : "+total_empWage;
 	}
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome To Employee Wage Calculator");
 		
-		calculateEmpWage("DMart", 20, 2, 10);
-		calculateEmpWage("Reliance", 10, 4, 20);
+		empWageCalculator dMart = new empWageCalculator("DMart", 20, 2, 10);
+		empWageCalculator reliance = new empWageCalculator("Reliance", 10, 4, 20);
+		dMart.calculateEmpWage();
+		System.out.println(dMart);
+		reliance.calculateEmpWage();
+		System.out.println(reliance);
 				
 	}
 }
