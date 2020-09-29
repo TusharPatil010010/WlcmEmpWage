79d9d4ff018ddbe6dca567d4680e41fd2658fec1
TusharPatil010010 <tusharp010010@gmail.com>
2020-09-29 22:41:16 +0530

    Added an interface approach to calculate employee wage

---
+++ calculateWageInterface.java
@@ -0,0 +1,5 @@
+public interface calculateWageInterface {
+
+	public void addCompanyEmpWage(String company, int emp_wage_per_hr, int max_working_days, int max_hrs_in_month);
+	public void computeEmpWage();
+}
--- empWageMultipleCompanies.java
+++ empWageMultipleCompanies.java
@@ -1,11 +1,28 @@
-public class empWageMultipleCompanies {
-	
-	//Global constants
+public class empWageMultipleCompanies implements calculateWageInterface {
+	//Global variables
 	public static final int is_part_time = 1;
 	public static final int is_full_time = 2;
 	
+	private int numOfCompany = 0;
+	private CompanyEmpWage[] companyEmpWageArray;
+		
+	public empWageBuilder() {
+		companyEmpWageArray = new CompanyEmpWage[5];
+	}
+	
+	private void addCompanyEmpWage(String company, int emp_wage_per_hr, int max_working_days, int max_hrs_in_month) {
+		companyEmpWageArray[numOfCompany] = new CompanyEmpWage(company, emp_wage_per_hr, max_working_days, max_hrs_in_month);
+		numOfCompany++;
+	}
+	
+	private void computeEmpWage() {
+		for(int i = 0; i < numOfCompany; i++) {
+			companyEmpWageArray[i].setTotalEmpWage(calculateEmpWage(companyEmpWageArray[i]));
+			System.out.println(companyEmpWageArray[i]);
+		}
+	}
 	//Method to calculate employee wage
-	public static int calculateEmpWage(String company, int emp_wage_per_hr,int max_working_days,int max_hrs_in_month) {
+	private int calculateEmpWage(CompanyEmpWage com) {
 		//variables
 		int empHrs = 0;
 		int total_empHrs = 0;
@@ -13,10 +30,9 @@ public class empWageMultipleCompanies {
 		int total_empWage = 0;
 		
 		//computation
-		while(total_empHrs <= max_hrs_in_month && total_working_days < max_working_days) {
+		while(total_empHrs <= com.max_hrs_in_month && total_working_days < com.max_working_days) {
 			total_working_days++;
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-			
 			switch(empCheck) {
 			case is_part_time:
 				empHrs = 8;
@@ -34,18 +50,17 @@ public class empWageMultipleCompanies {
 		
 		//if total hrs exceeds max hrs per month then computing for max hrs per month
 		
-		System.out.println("Total emp hrs : "+total_empHrs);
-		total_empWage = total_empHrs * emp_wage_per_hr;
-		System.out.println("Total Emp wage for Company '"+company+"' is : "+total_empWage);
-				
+		total_empWage = total_empHrs * com.emp_wage_per_hr;
 		return total_empWage;
 	}
+	
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome To Employee Wage Calculator");
 		
-		calculateEmpWage("DMart", 20, 2, 10);
-		calculateEmpWage("Reliance", 10, 4, 20);
-				
+		calculateWageInterface ewb = new empWageBuilder();
+		ewb.addCompanyEmpWage("DMart", 20, 2, 10);
+		ewb.addCompanyEmpWage("Reliance", 10, 4, 20);
+		ewb.computeEmpWage();	
 	}
 }
